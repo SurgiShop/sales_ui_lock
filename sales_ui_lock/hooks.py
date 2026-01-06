cb596bad68fcea5b69f34e11afbba1646a740c98
garystarr-surgi <gary.starr@surgishop.com>
2026-01-06 08:20:13 -0500

    Update hooks.py

--- sales_ui_lock/hooks.py
+++ sales_ui_lock/hooks.py
@@ -1,1 +1,11 @@
+app_name = "sales_ui_lock"
+app_title = "Sales UI Lock"
+app_publisher = "SurgiShop"
+app_description = "Force roles to their specific module and eliminate access to desktop"
+app_email = "gary.starr@surgishop.com"
+app_license = "MIT"
+
+app_include_js = [
+    "/assets/sales_ui_lock/js/desk_ui_lock.js"
+]
 
