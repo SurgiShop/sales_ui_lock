24894acec6057e503197277d11db5cb92a20d28c
garystarr-surgi <gary.starr@surgishop.com>
2026-01-23 11:58:43 -0500

    Update hooks.py

--- sales_ui_lock/hooks.py
+++ sales_ui_lock/hooks.py
@@ -12,31 +12,3 @@ app_include_js = [
     "/assets/sales_ui_lock/js/desk_ui_lock.js"
 ]
 
-fixtures = [
-    {
-        "dt": "Workspace",
-        "filters": [["name", "=", "Sales User"]]
-    }
-]
-
-add_to_apps_screen = [
-	{
-		"name": "sales_ui_lock",
-		"logo": "/assets/sales_ui_lock/sales_ui_lock/sales-ui-lock-icon.svg",
-		"title": "Sales User",
-		"route": "/app/Sales User",
-		"has_permission": "erpnext.check_app_permission",
-	}
-]
-
-# before_install = "sales_ui_lock.install.before_install"
-after_install = "sales_ui_lock.sales_ui_lock.install.after_install"
-
-# Ensure workspace sidebar is set up after migrations
-after_migrate = [
-	"sales_ui_lock.sales_ui_lock.install.ensure_workspace_exists",
-	"sales_ui_lock.sales_ui_lock.install.ensure_sales_user_sidebar",
-	"sales_ui_lock.sales_ui_lock.install.cleanup_old_workspaces",
-	"sales_ui_lock.sales_ui_lock.install.fix_settings_defaults",
-	"sales_ui_lock.sales_ui_lock.workspace_sidebar.ensure_sales_sidebar"
-]
