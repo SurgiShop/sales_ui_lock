e91b8e29968a5ecac52fbc79bdb59d40d670a122
garystarr-surgi <gary.starr@surgishop.com>
2026-02-10 10:30:06 -0500

    Remove boot_session from hooks.py

--- sales_ui_lock/hooks.py
+++ sales_ui_lock/hooks.py
@@ -9,9 +9,6 @@ required_frappe_version = ">=16.0.0"
 app_logo_url = "/assets/sales_ui_lock/sales_ui_lock/sales-ui-lock-icon.svg"
 
 
-boot_session = "sales_ui_lock.boot.enforce_allowed_modules"
-
-
 app_include_js = [
     "/assets/sales_ui_lock/js/desk_ui_lock.js",
     "/assets/sales_ui_lock/js/disable_navbar_items.js"
