1e0f6dd8b9cf315886ab30b03c899292d2082d90
garystarr-surgi <gary.starr@surgishop.com>
2026-02-10 12:23:03 -0500

    Update hooks.py

--- sales_ui_lock/hooks.py
+++ sales_ui_lock/hooks.py
@@ -8,29 +8,8 @@ required_apps = ["frappe/erpnext"]
 required_frappe_version = ">=16.0.0"
 app_logo_url = "/assets/sales_ui_lock/sales_ui_lock/sales-ui-lock-icon.svg"
 
-# ======================
-# BOOT SESSION HOOK
-# ======================
-boot_session = "sales_ui_lock.boot.enforce_allowed_modules"
 
-# ======================
-# CLIENT SIDE JS
-# ======================
 app_include_js = [
     "/assets/sales_ui_lock/js/desk_ui_lock.js",
     "/assets/sales_ui_lock/js/disable_navbar_items.js"
 ]
-
-# ======================
-# SERVER HOOKS
-# ======================
-# ======================
-# SERVER HOOKS
-# ======================
-# after_migrate = [
-    # "sales_ui_lock.workspace_sidebar.setup_sales_workspace_sidebar"
-]
-
-
-# optionally, for new installs only
-# after_install = "sales_ui_lock.workspace_sidebar.setup_sales_workspace_sidebar"
